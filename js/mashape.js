f91c7e638d41790c3fbd3e83f6608017ad060532
OSP123 <omarpatel123@gmail.com>
2016-05-11 13:27:11 -0700

    added beginning of Mashape tutorial

---
+++ js/mashape.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+	var appID = "WiNaHz5SylmshT0YdDN3OQxNVf5np1xQoxNjsnpmJSREuIgvnp";
+
+    var someString = "I am a lovely string.";
+    var encode = encodeURI(someString);
+
+    console.log(encode);
+
+    $.ajax({
+        url: 'https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+        type: 'POST', // The HTTP Method
+        data: {}, // Additional parameters here
+        datatype: 'json',
+        success: function(data) { alert(JSON.stringify(data)); },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", appID); // Enter your Mashape key here
+        }
+    });
+
+});
