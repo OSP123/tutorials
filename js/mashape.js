0a6fdf8780445e7ae4f3bc47017ef6afba15a314
OSP123 <omarpatel123@gmail.com>
2016-05-11 13:29:04 -0700

    added beginning of Mashape tutorial

--- js/mashape.js
+++ js/mashape.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
     console.log(encode);
 
     $.ajax({
-        url: 'https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+        url: 'https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/' + encode, // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
         type: 'POST', // The HTTP Method
         data: {}, // Additional parameters here
         datatype: 'json',
