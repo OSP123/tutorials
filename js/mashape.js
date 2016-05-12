e5c80feafee6d20a46a6af29347ba64ee3b5f98c
OSP123 <omarpatel123@gmail.com>
2016-05-11 18:53:01 -0700

    Yoda Talk API now working

--- js/mashape.js
+++ js/mashape.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-	$(".text_process").click(function(){
+	$(".yoda_button").click(function(){
 
         $.ajax({
             url: 'https://yoda.p.mashape.com/yoda?sentence=', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
@@ -19,5 +19,27 @@ $( document ).ready(function() {
         });
 
     });
+
+    $(".text_process_button").click(function(){
+
+        $.ajax({
+            url: 'https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/?text=', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+            type: 'GET', // The HTTP Method
+            data: {text: $("#text_process_input").val()}, // Additional parameters here
+            datatype: 'json',
+            success: function (data) {
+                $("#output").html(data);
+            },
+            error: function (err) {
+                alert(err);
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("X-Mashape-Authorization", "WiNaHz5SylmshT0YdDN3OQxNVf5np1xQoxNjsnpmJSREuIgvnp"); // Enter here your Mashape key
+            }
+        });
+
+    });
+
+    
 
 });
