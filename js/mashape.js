4986e688eba13e8e593caca40ecb4f533cf877cd
OSP123 <omarpatel123@gmail.com>
2016-05-11 17:04:42 -0700

    trying to make first mashape work

--- js/mashape.js
+++ js/mashape.js
@@ -1,21 +1,32 @@
 $( document ).ready(function() {
-	var appID = "WiNaHz5SylmshT0YdDN3OQxNVf5np1xQoxNjsnpmJSREuIgvnp";
 
-    var someString = "I am a lovely string";
-    var converted = someString.replace(/ /g, "+");
+	$(".text_process").click(function(){
 
-    $.ajax({
-        url: 'https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/?text=' + converted, // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-        type: 'POST', // The HTTP Method
-        data: {}, // Additional parameters here
-        datatype: 'json',
-        success: function(data) { console.log(JSON.stringify(data)); },
-        error: function(err) { console.log(err); },
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("X-Mashape-Authorization", appID); // Enter your Mashape key here
-        }
+    var input = $('input')[0].val();
+        $.getJSON("//loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/?text=" + input, function( data ) {
+           $('#output').append(data.contents.translated);
+        });
     });
 
-    console.log(converted);
+    // $(".query_btn").click(function(){
+
+    //     var query_param = $(this).prev().val();
+
+    //     if ($(this).prev().attr("placeholder") == "City") {
+    //         var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&units=imperial&APPID=" + appID;
+    //     } else if ($(this).prev().attr("placeholder") == "Zip Code") {
+    //         var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&units=imperial&APPID=" + appID;
+    //     } 
+
+    //     $.getJSON(weather,function(json){
+    //         $("#city").html(json.name);
+    //         $("#main_weather").html(json.weather[0].main);
+    //         $("#description_weather").html(json.weather[0].description);
+    //         $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+    //         $("#temperature").html(json.main.temp);
+    //         $("#pressure").html(json.main.pressure);;
+    //         $("#humidity").html(json.main.humidity);;
+    //     });
+    // })
 
 });
