ecba3151f2753548ccb751e0de8e4a2a7148c373
OSP123 <omarpatel123@gmail.com>
2016-05-11 18:35:42 -0700

    trying to make first mashape work

--- js/mashape.js
+++ js/mashape.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
         $.ajax({
             url: 'https://yoda.p.mashape.com/yoda?sentence=', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
             type: 'GET', // The HTTP Method
-            data: {sentence: $("#text_process_input").val()}, // Additional parameters here
+            data: {sentence: $("#yoda_input").val()}, // Additional parameters here
             datatype: 'json',
             success: function (data) {
                 $("#output").html(data);
