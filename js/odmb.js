05759ae4a1956d85c1d02bc400430df8a87fb28f
OSP123 <omarpatel123@gmail.com>
2016-05-12 21:33:06 -0700

    Trying to make yoda thing work

---
+++ js/odmb.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+	$("#omdb_submit").click(function(){
+		//Gather values from the other fields and store them into variables.
+		var title = $("#title").val();
+		var year = $("#year").val(); 
+		// Concatenate those variables to a query string.
+		var queryString = "http://www.omdbapi.com/?t=" + title + "&y=" + year + "&plot=short&r=json";
+		// Put that query string into the AJAX request
+		$.ajax({
+            url: '//yoda.p.mashape.com/yoda', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+            method: 'GET'
+        }).done(function(response) {
+        	$("#output").html(response);
+        });
+		// Output data into output container
+
+	});
+});
